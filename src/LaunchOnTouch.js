dfe0afd8fe8ce98485f8201aa3f7b0e5fa5f4e7a
aqphungngoc <aqphungngoc@gmal.com>
2018-01-27 21:36:25 +0700

    aas

--- src/LaunchOnTouch.js
+++ src/LaunchOnTouch.js
@@ -1,25 +1,22 @@
 var LaunchOnTouch = cc.Node.extend({
-	_launch : null,
-	ctor : function(){
-		var that = this;
-		this._super();
-		cc.eventManager.addListener(
-				{
-					event: cc.EventListener.TOUCH_ONE_BY_ONE,
-					swallowTouches: true,
-					onTouchBegan: function (touch, event) {
+    _launch: null,
+    ctor: function() {
+        var that = this;
+        this._super();
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches: true,
+            onTouchBegan: function(touch, event) {
+                console.log(touch);
+                if (that.touchInViewport(touch) && that._launch != null) {
+                    that._launch();
+                }
+                return true;
+            },
+        }, this);
+    },
 
-						if (that.touchInViewport(touch) && that._launch != null){
-							that._launch();
-						}
-						return true;
-					},
-				}
-				, this
-				);
-	},
-
-	touchInViewport: function(touch){
-		return true;
-	}
+    touchInViewport: function(touch) {
+        return true;
+    }
 });
